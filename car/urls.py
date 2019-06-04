8643157f03799467d43ddb328d39f37c4772ee44
Viachesav Kravchenko <edocsil@knu.ua>
2019-06-05 01:25:53 +0300

    Added Google and Facebook auth

--- car/urls.py
+++ car/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls import url, include
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('accounts/registration/', views.RegisterFormView.as_view(), name='registration'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
     path('story_list/', views.story_list, name='story_list'),
     path('story_list/story/<int:pk>/', views.story_detail, name='story_detail'),
     path('story_add/', views.story_add, name='story_add'),
